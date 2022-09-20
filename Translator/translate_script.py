ead5742b35fb7a494b81d576a217e4f977aebd75
Kevin Pham <phamtkevin@gmail.com>
2022-09-19 20:49:15 -0700

    English to Korean Translator

--- Translator/translate_script.py
+++ Translator/translate_script.py
@@ -7,5 +7,8 @@ try:
         text = my_file.read()
         translation = translator.translate(text)
         print(translation)
+        # Write the translation to a new file
+        with open('./translated-ko.txt', mode='w', encoding='utf-8') as my_translated_file:
+            my_translated_file.write(translation)
 except FileNotFoundError as err:
     print('Check file path')
