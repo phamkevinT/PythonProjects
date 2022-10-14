1a9808a103523ec5730a0df407b5a1fd6bcac56f
Kevin Pham <phamtkevin@gmail.com>
2022-10-13 23:21:47 -0700

    Flask templates and adding favicon

--- Portfolio/server.py
+++ Portfolio/server.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+# Run using 'flask --app <filename> --debug run'
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
