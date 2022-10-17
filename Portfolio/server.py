2754219b5cb7ba8fbfbc57830ddcc028b9b29624
Kevin Pham <phamtkevin@gmail.com>
2022-10-16 19:17:53 -0700

    Added HTML template

--- Portfolio/server.py
+++ Portfolio/server.py
@@ -10,6 +10,11 @@ def hello_world():
     return render_template('index.html')
 
 
+@app.route("/<username>/<int:post_id>")
+def hello_user(username=None, post_id=None):
+    return render_template('index.html', name=username, post_id=post_id)
+
+
 @app.route("/about")
 def about():
     return render_template('about.html')
